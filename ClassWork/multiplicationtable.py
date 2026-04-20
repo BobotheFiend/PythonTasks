4adbe5b4d71112a47529bbeafb1e762e04b23a76
Nnamdi <cavennnamdi@gmail.com>
2026-04-20 10:37:57 +0100

    Multiplicationtimes table

---
+++ ClassWork/multiplicationtable.py
@@ -0,0 +1,14 @@
+"""
+collect a number
+initalize a count
+multiply the number and the amount of times the loop runs
+"""
+
+number = int(input('Enter a number to multiply: '))
+multiply = 1;
+count = 1;
+        
+for count in range(1,11):
+    multiply = number*count
+    print(number, " x ", count, " = ", multiply)
+
