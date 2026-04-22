71dbb9cee9d93d1fa864e3ace2404049da57bd35
Nnamdi <cavennnamdi@gmail.com>
2026-04-22 09:20:28 +0100

    reverse the string

---
+++ Assignment/tearsofjoy/reversestring.py
@@ -0,0 +1,9 @@
+#13
+
+user_input = input(" enter a letters: ")
+reversing_the_string = " "
+for name in (user_input):
+    reversing_the_string = name + reversing_the_string
+
+print(f"{reversing_the_string}", end="")
+print()
